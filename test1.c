c0d2eeb1172b8f79889e4e3b37c013f2c450ddc5
maobin <2535703804@qq.com>
2026-08-14 08:40:05 +0800

    测试提交C运行库

--- test1.c
+++ test1.c
@@ -6,11 +6,19 @@
  * @brief 测试函数
  * @param a: 参数a
  * @param b: 参数b
+ * @return 无
  */
 void test_1(const int a, const int b) {
-    printf("test-a->%d, test-b->%d", a, b);
+    printf("test-a->%d, test-b->%d", a+1, b+1);
 }
 
+
+/**
+ * @brief 测试函数
+ * @param a
+ * @param b
+ * @return 无
+ */
 void test_2(const int a, const int b) {
-    printf("test-a->%d, test-b->%d", a, b);
+    printf("test-a->%d, test-b->%d", a+2, b+2);
 }
